f280820d1674ed894ee513ccb78cc1f566293e2f
Anastasiia Bakai <anastasiia.bakai@gmail.com>
2019-07-12 16:16:02 +0400

    sidebar toggle

--- dist/assets/js/robonomics-html-init.js
+++ dist/assets/js/robonomics-html-init.js
@@ -4,8 +4,6 @@ document.addEventListener('DOMContentLoaded',function(){
 	//popper.js, tooltip.js
   	var reference = document.querySelectorAll('.js-tooltip');
 
-  	console.log(reference);
-
   	if ( reference )
   		{
   			reference.forEach(function(e){
--- dist/assets/js/robonomics-html.js
+++ dist/assets/js/robonomics-html.js
@@ -122,35 +122,96 @@ window.addEventListener('load', function(){
 	document.addEventListener('resize', IfInView);
 
 }, false);
-var selector = 'data-toggle',
+var 
+	selector = 'on-toggle',
+	onclose  = 'on-close',
 	classOpen = 'open',
 	classSwitchOn = 'on';
 
-function action(e){
 
-	var open, close;
+// function close(e){
+// 	var o, link;
 
-	e.classList.toggle(classSwitchOn);
-	o = document.querySelectorAll( e.getAttribute(selector) );
+// 	o = document.querySelectorAll( e.getAttribute(onclose) );
+// 	o.forEach(function(i){
+// 		i.classList.remove(classOpen);
+// 	});
 
-	o.forEach(function(i){
-		i.classList.toggle(classOpen);
-	});
-}
+// 	link = document.querySelectorAll('[' + selector + ']');
+// 	link.forEach(function(i){
+// 		i.classList.remove(classSwitchOn);
+// 	});
+// }
+
+
+// function open(e){
+
+// 	e.classList.toggle(classSwitchOn);
+
+// 	o = document.querySelectorAll( e.getAttribute(selector) );
+// 	o.forEach(function(i){
+// 		i.classList.toggle(classOpen);
+// 	});
+// }
 
 
 function init(){
 
-	var e = document.querySelectorAll('[' + selector + ']');
+	var e = document.querySelectorAll('.js-sidebar-link');
+	var c = document.querySelectorAll('.js-sidebar-content');
 
 	e.forEach(function(i){
-
 		i.addEventListener("click", function(event){
 			event.preventDefault();
-			action(this);
-		});
 
+			var
+				t = this,
+				contentID = t.getAttribute(selector);
+
+			e.forEach(function(i){
+				if (i != t){
+					i.classList.remove(classSwitchOn);
+				}
+				else{
+					if (t.classList.contains(classSwitchOn))
+						t.classList.remove(classSwitchOn);
+					else
+						t.classList.add(classSwitchOn);
+				}
+			});
+
+
+			c.forEach(function(i){
+
+				var id = '#'+i.id;
+
+				if (id != contentID ){
+					i.classList.remove(classOpen);
+				}
+				else{
+					if (i.classList.contains(classOpen))
+						i.classList.remove(classOpen);
+					else
+						i.classList.add(classOpen);
+
+				}
+			});
+
+
+		});
 	});
+
+	// var e = document.querySelectorAll('[' + selector + ']');
+
+	// e.forEach(function(i){
+
+	// 	i.addEventListener("click", function(event){
+	// 		event.preventDefault();
+	// 		close(this);
+	// 		open(this);
+	// 	});
+
+	// });
 }
 
 
